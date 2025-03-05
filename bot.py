a97b9677388b37b8c142043d6ed77749b9aace7f
unique <willsmithlikescat24@gmail.com>
2025-03-05 11:16:38 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from config.config import TOKEN
 from events.on_ready import setup as setup_on_ready
 from commands.random import setup as setup_random
 from commands.quotes import setup as setup_quotes
+from commands.dice import setup as setup_dice
 
 # Create a bot instance with intents
 intents = discord.Intents.default()
@@ -14,6 +15,7 @@ async def main():
     await setup_on_ready(bot)  # Await the setup coroutine for on_ready
     await setup_random(bot)    # Await the setup coroutine for random command
     await setup_quotes(bot)  # Await the setup coroutine for quotes command
+    await setup_dice(bot)    # Await the setup coroutine for dice command
     await bot.start(TOKEN)     # Start the bot
 
 # Run the bot
