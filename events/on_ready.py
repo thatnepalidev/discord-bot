25047c668af180ad34b3e8beadb3ed0e58fed17c
unique <willsmithlikescat24@gmail.com>
2025-03-13 15:05:08 +0000

    Update on_ready.py

--- events/on_ready.py
+++ events/on_ready.py
@@ -5,5 +5,12 @@ async def setup(bot: commands.Bot):
     @bot.event
     async def on_ready():
         print(f'Logged in as {bot.user.name}')
+        await bot.change_presence(
+            status=discord.Status.online,
+            activity=discord.Activity(
+                type=discord.ActivityType.watching,
+                name="Planting flowers 🌼"
+            )
+        )
         await bot.tree.sync()  # Sync commands with Discord
         print("Commands synced.")
