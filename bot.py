5e68a96d2145c163c2527a920abdf485c5c6c907
unique <willsmithlikescat24@gmail.com>
2025-03-13 15:05:56 +0000

    Add files via upload

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from events.on_ready import setup as setup_on_ready
 from commands.random import setup as setup_random
 from commands.quotes import setup as setup_quotes
 from commands.dice import setup as setup_dice
+from commands.translate import setup as setup_translate
 
 # Create a bot instance with intents
 intents = discord.Intents.default()
@@ -16,6 +17,7 @@ async def main():
     await setup_random(bot)    # Await the setup coroutine for random command
     await setup_quotes(bot)  # Await the setup coroutine for quotes command
     await setup_dice(bot)    # Await the setup coroutine for dice command
+    await setup_translate(bot)  # Await the setup coroutine for translate command
     await bot.start(TOKEN)     # Start the bot
 
 # Run the bot
